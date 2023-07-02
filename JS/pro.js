4a0e9b6062346a599c53f0155442765f19e7216a
AmanShukla3074 <136250722+AmanShukla3074@users.noreply.github.com>
2023-07-02 12:39:26 +0530

    Add files via upload

---
+++ JS/pro.js
@@ -0,0 +1,18 @@
+var Mimg = document.getElementById('mainimg');
+var Simg = document.getElementsByClassName('sm-img');
+
+Simg[0].onclick = function(){
+    Mimg.src=Simg[0].src;
+}
+Simg[1].onclick = function(){
+    Mimg.src=Simg[1].src;
+}
+Simg[2].onclick = function(){
+    Mimg.src=Simg[2].src;
+}
+Simg[3].onclick = function(){
+    Mimg.src=Simg[3].src;
+}
+Simg[4].onclick = function(){
+    Mimg.src=Simg[4].src;
+}
